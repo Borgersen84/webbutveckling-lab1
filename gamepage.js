031ab016785274d762b688d7ac79dcf25e5dc9c3
Öyvind <oybo84@gmail.com>
2019-04-05 13:24:11 +0200

    Functionality for loading in answer options added

--- gamepage.js
+++ gamepage.js
@@ -1,7 +1,10 @@
-const question = document.getElementById('question');
+const question = document.getElementById("question");
+const answers = Array.from(document.getElementsByClassName("answer-text"));
 
 let questions = [];
 let remainingQuestions = [];
+let incorrectAnswers = [];
+let allAnswers = [];
 
 
 // Fetching request from api
@@ -31,6 +34,41 @@ var url = "https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&typ
 		remainingQuestions = [... questions];
 		console.log(remainingQuestions);
 		const randomIndex = Math.floor(Math.random() * remainingQuestions.length);
-		console.log(remainingQuestions[randomIndex].question);
-		question.innerText = remainingQuestions[randomIndex].question;
+		const randomAnswerIndex = Math.floor(Math.random() * 3);
+		let currentQuestion = remainingQuestions[randomIndex].question;
+		question.innerText = currentQuestion;
+		let correctAnswer = remainingQuestions[randomIndex].correct_answer;
+		incorrectAnswers = remainingQuestions[randomIndex].incorrect_answers;
+		allAnswers = [... incorrectAnswers];
+		allAnswers.splice(randomAnswerIndex, 0, correctAnswer);
+		console.log(currentQuestion);
+		console.log(correctAnswer);
+		console.log(incorrectAnswers);
+		console.log(allAnswers);
+		answers[0].innerText = allAnswers[0];
+		answers[1].innerText = allAnswers[1];
+		answers[2].innerText = allAnswers[2];
+		answers[3].innerText = allAnswers[3];
+		
+		answers.forEach(function(answer){
+			answer.addEventListener("click", e => {
+				const chosenAnswer = e.target;
+				if(chosenAnswer === answers[0] && answers[0].innerText === correctAnswer){
+					console.log("Det funker");
+				}
+				else if(chosenAnswer === answers[1] && answers[1].innerText === correctAnswer){
+					console.log("Det funker");
+				}
+				else if(chosenAnswer === answers[2] && answers[2].innerText === correctAnswer){
+					console.log("Det funker");
+				}
+				else if(chosenAnswer === answers[3] && answers[3].innerText === correctAnswer){
+					console.log("Det funker");
+				}
+				else {
+					console.log("fel svar");
+				}
+			})
+		});
+		
 	}
