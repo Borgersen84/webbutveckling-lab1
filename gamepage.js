50e1e2476ab9fa3fa996093b74622ff53a858d8f
Öyvind <oybo84@gmail.com>
2019-04-05 23:15:26 +0200

    clean up of code and comments added

--- gamepage.js
+++ gamepage.js
@@ -40,63 +40,59 @@ var url = "https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&typ
     xhr.send();
 	
 	function gameStarter() {
+		// copying content to new array so we can edit it and keep the old as it is
 		remainingQuestions = [... questions];
 		showQuestion();
 	}
 	
 	function showQuestion() {
-
+		
+		//go to endpage and save score locally when there is no more questions in the array
 		if(remainingQuestions.length === 0){
 			localStorage.setItem("finalScore", score);
 			return window.location.assign("endpage.html");
 		}
+		//fills up progressbar for every question generated
 		progressBar.style.width = `${(questionCounter / 10) * 100}%`;
 		questionNumber.innerText = questionCounter + "/" + 10;
-		console.log(remainingQuestions);
+		// Generates a random index so we can use it to get a random question from our array
 		const randomIndex = Math.floor(Math.random() * remainingQuestions.length);
 		const randomAnswerIndex = Math.floor(Math.random() * 4);
 		currentQuestion = remainingQuestions[randomIndex].question;
 		question.innerText = currentQuestion;
+		// Gets the correct answer so we can use it to compare players choice
 		correctAnswer = remainingQuestions[randomIndex].correct_answer;
 		incorrectAnswers = remainingQuestions[randomIndex].incorrect_answers;
 		allAnswers = [... incorrectAnswers];
+		// puts the correct answer in a random position with the incorrect answers
 		allAnswers.splice(randomAnswerIndex, 0, correctAnswer);
-		console.log(currentQuestion);
-		console.log(correctAnswer);
-		console.log(incorrectAnswers);
-		console.log(allAnswers);
+		// makes our answer options visible on the page
 		answers[0].innerText = allAnswers[0];
 		answers[1].innerText = allAnswers[1];
 		answers[2].innerText = allAnswers[2];
 		answers[3].innerText = allAnswers[3];
+		// removes question from array so it doesn't repeat itself during the game
 		remainingQuestions.splice(randomIndex, 1);
 	};
 	
+	// checks and gives points for correct answer
 	answers.forEach(function(answer){
 			answer.addEventListener("click", e => {
 				const chosenAnswer = e.target;
 				if(chosenAnswer === answers[0] && answers[0].innerText === correctAnswer){
-					console.log("Det funker");
 					score += points;
 				}
 				else if(chosenAnswer === answers[1] && answers[1].innerText === correctAnswer){
-					console.log("Det funker");
 					score += points;
 				}
 				else if(chosenAnswer === answers[2] && answers[2].innerText === correctAnswer){
-					console.log("Det funker");
 					score += points;
 				}
 				else if(chosenAnswer === answers[3] && answers[3].innerText === correctAnswer){
-					console.log("Det funker");
 					score += points;
 				}
-				else {
-					console.log("fel svar");
-				}
-				
+
 				questionCounter++;
-				console.log(score);
 				scoreNumber.innerText = score;
 				showQuestion();
 			})
